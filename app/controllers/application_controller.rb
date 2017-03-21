511a6b2a8c06fdd6a4df51da992bf212e94b1cb4
Mike Kuo <michaelchunkuo@gmail.com>
2017-03-21 13:43:03 -0700

    Add user creation to root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,32 @@
 class ApplicationController < ActionController::API
-  def index
-    render plain: "Hello world!", status: 200
+
+  before_action :check_params
+
+  def create
+    if User.where(email: params[:email]).any?
+      render_unauthorized
+    else
+      user = User.new(email: params[:email])
+      user.save
+      render_user(user)
+    end
+  end
+
+  protected
+
+  def check_params
+    render_malformed if params[:email] == nil && params[:user_id] == nil
+  end
+
+  def render_malformed
+    render plain: "Malformed request.", status: 400
+  end
+
+  def render_unauthorized
+    render plain: "Unauthorized credentials.", status: 403
+  end
+
+  def render_user(user)
+    render json: user, status: 200
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  post '/' => "application#index", as: "root"
+  post '/' => "application#create", as: "root"
 end
