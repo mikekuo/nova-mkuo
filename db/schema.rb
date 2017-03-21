b7073fad1a8730d24fbba29b6e05b7252bb3f86a
Mike Kuo <michaelchunkuo@gmail.com>
2017-03-21 12:37:11 -0700

    Create team model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321193217) do
+ActiveRecord::Schema.define(version: 20170321193547) do
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "token"
